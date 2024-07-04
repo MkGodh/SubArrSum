25fc468ab755713d7b63e65a003e41eb2f4a4781
MkaGodh <fenryrvla@yandex.ru>
2024-07-04 15:49:39 +0300

    Добавьте файлы проекта.

---
+++ SubArrSum/SubArrSum.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> vec = { -4, 3, 5, -4, 9, 1, -3, -3, 1 };
+    int max = 0;
+    int end = 0;
+    int curInd = 0;
+    int curIndTwo = 0;
+
+    for (int i = 0; i < vec.size(); i++) {
+
+        for (int j = i; j < vec.size(); j++) {
+            max += vec[j];
+            if (max > end)
+            {
+                curInd = i;
+                curIndTwo = j;
+                end = max;
+            }
+        }
+        max = 0;
+    }
+
+    std::cout << curInd << " " << curIndTwo;
+    return 0;
+}
+
